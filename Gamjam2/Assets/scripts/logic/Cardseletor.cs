945b6946e313d8bb90c781787758c2967f615a8c
Mads henriksen <mads@realitygaminggroup.com>
2021-12-27 16:59:24 +0100

    ved ikke

---
+++ Gamjam2/Assets/scripts/logic/Cardseletor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Model;
+using System;
+public class Cardseletor : MonoBehaviour
+{
+   public Card CoruntCard;
+
+    public Deck deck;
+
+
+    public void trakkort()
+    {
+
+        deck.FaceUp.Add( deck.Facedown.First());
+        CoruntCard = deck.Facedown.First();
+        deck.Facedown.Remove(deck.Facedown.First());
+
+    }
+
+
+    public  void FaceDownShoffel()
+    {
+       
+        List<Card> midListe = new List<Card>();
+
+       System.Random ran = new System.Random();
+       midListe = deck.Facedown.OrderBy(a => ran.Next()).ToList();
+}
+
+
+
+}
---
+++ Gamjam2/Assets/scripts/model/Deck.cs
@@ -0,0 +1,12 @@
+using Model;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public List<Card> Facedown;
+    public List<Card> FaceUp;
+
+
+}
