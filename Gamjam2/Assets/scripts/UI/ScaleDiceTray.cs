bfd4f09df5d99c0f625f28a3a1f980ead68843f1
Krapylet <aske.j_hotmail.com@users-noreply.github.com>
2021-12-27 21:48:10 +0100

    saved main scene

--- Gamjam2/Assets/scripts/UI/ScaleDiceTray.cs
+++ Gamjam2/Assets/scripts/UI/ScaleDiceTray.cs
@@ -5,22 +5,6 @@ using UnityEngine.UI;
 
 public class ScaleDiceTray : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-
-    // 1000/1000 = 5.75/5.75
-
-    // Når forholdet mellem højde og brede ændre sig i pixels skal det tilsværende ændres i units.
-
-    // heigh of 660 = scale of 5.77f
-    // width of 930 = scale of 8.135f
-
-    // height of 380 = scale of 5.735f
-    // width of 930 = scale of 14f
-    static float basePixelsToUnitScale = 5.75f / 1000f;
-
-    //static float pixelsToUnitScale = 14f / 930f; // = 0.015f
-
     public RectTransform canvasRectTransform;
 
     //Die box transforms
@@ -30,8 +14,9 @@ public class ScaleDiceTray : MonoBehaviour
     public Transform left;
     public Transform right;
 
+    //constants
     static float dieBoxThickness = 0.1f;
-    static float cameraDist = -5f;
+    static float cameraDist = -6f;
 
     void Update()
     {
